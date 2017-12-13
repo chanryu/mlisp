35e0a9cb0bd375dfdb0e88d6431b75ad5a9d4296
Chan Ryu <cryu@snapchat.com>
2017-12-13 23:57:33 +1100

    enhance test.lisp

--- test.lisp
+++ test.lisp
@@ -1,15 +1,19 @@
-(set 'l (lambda  () (print 'lambda:  a)))
-(set 'c (closure () (print 'closure: a)))
+(set 'l (lambda () a))
+(set 'c (closure () a))
 
 (set 'a 1)
-(l)
-(c)
+(print 'set-a-to-1)
+(print 'lambda-expected: 1 'actual: (l))
+(print 'closure-expected: 1 'actual: (c))
 
 (set 'a 2)
-(l)
-(c)
+(print 'set-a-to-2)
+(print 'lambda-expected: 2 'actual: (l))
+(print 'closure-expected: 2 'actual: (c))
 
 (do
+	(print 'new-scope-begin)
   (set 'a 1)
-  (l)
-  (c))
+	(print 'set-a-to-1)
+  (print 'lambda-expected: 1 'actual: (l))
+  (print 'closure-expected: 2 'actual: (c)))
