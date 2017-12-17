b5b11527d9b577270c58054c5ecb17b4e7b9c5b4
Chan Ryu <cryu@snapchat.com>
2017-12-17 23:21:50 +1100

    fix lambda

--- test.lisp
+++ test.lisp
@@ -1,19 +1,15 @@
 (set 'l (lambda () a))
-(set 'c (closure () a))
 
-(set 'a 1)
 (print 'set-a-to-1)
+(set 'a 1)
 (print 'lambda-expected: 1 'actual: (l))
-(print 'closure-expected: 1 'actual: (c))
 
-(set 'a 2)
 (print 'set-a-to-2)
+(set 'a 2)
 (print 'lambda-expected: 2 'actual: (l))
-(print 'closure-expected: 2 'actual: (c))
 
 (do
-	(print 'new-scope-begin)
+  (print 'new-scope-begin)
   (set 'a 1)
-	(print 'set-a-to-1)
-  (print 'lambda-expected: 1 'actual: (l))
-  (print 'closure-expected: 2 'actual: (c)))
+  (print 'set-a-to-1)
+  (print 'lambda-expected: 2 'actual: (l)))
