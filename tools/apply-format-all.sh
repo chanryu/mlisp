7a4dc5ece61eb7cfbfec8ae74c230c2e4cbc5ed1
Chan Ryu <upriser@gmail.com>
2019-11-07 12:50:53 +1100

    clang-format (cont'd)

---
+++ tools/apply-format-all.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+scriptDir="$(cd "$(dirname "$0")" && pwd -P)"
+find "src" -type f -print | xargs "${scriptDir}"/apply-format.sh
--- tools/clang-format-all.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-scriptDir="$(cd "$(dirname "$0")" && pwd -P)"
-find "src" -type f -print | xargs "${scriptDir}"/clang-format.sh
