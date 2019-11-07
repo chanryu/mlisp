be87731dd100111cf37d02cabba93190ca8d2610
Chan Ryu <upriser@gmail.com>
2019-11-07 12:40:55 +1100

    clang-format (cont'd)

--- src/operators.cpp
+++ src/operators.cpp
@@ -11,10 +11,10 @@
 
 #include "load.hpp"
 
-#define MLISP_DEFUN(cmd__, proc__)                                                                                     \
+#define MLISP_DEFUN(cmd__, func__)                                                                                     \
     do {                                                                                                               \
         char const* cmd = cmd__;                                                                                       \
-        env.set(cmd, proc__);                                                                                          \
+        env.set(cmd, make_proc(func__));                                                                               \
     } while (0)
 
 using namespace std::string_literals;
@@ -178,270 +178,270 @@ void set_primitive_procs(mll::Env& env)
     using namespace mll;
 
     // "quote" is already built into the Parser/eval()
-    // env->set("quote", make_proc([](List args, Env) {
+    // env->set("quote", [](List args, Env) {
     //    return car(args);
-    //}));
+    //});
 
-    MLISP_DEFUN("atom", make_proc([cmd](List args, Env& env) {
-                    assert_argc(args, 1, cmd);
-                    auto list = dynamic_node_cast<List>(eval(car(args), env));
-                    return to_node(!list || list->empty());
-                }));
+    MLISP_DEFUN("atom", [cmd](List args, Env& env) {
+        assert_argc(args, 1, cmd);
+        auto list = dynamic_node_cast<List>(eval(car(args), env));
+        return to_node(!list || list->empty());
+    });
 
-    MLISP_DEFUN("eq", make_proc([cmd](List args, Env& env) {
-                    assert_argc(args, 2, cmd);
-                    auto lhs = eval(car(args), env);
-                    auto rhs = eval(cadr(args), env);
-                    return to_node(lhs.data() == rhs.data());
-                }));
+    MLISP_DEFUN("eq", [cmd](List args, Env& env) {
+        assert_argc(args, 2, cmd);
+        auto lhs = eval(car(args), env);
+        auto rhs = eval(cadr(args), env);
+        return to_node(lhs.data() == rhs.data());
+    });
 
-    MLISP_DEFUN("car", make_proc([cmd](List args, Env& env) {
-                    assert_argc(args, 1, cmd);
-                    return car(to_list_or_throw(eval(car(args), env), cmd));
-                }));
+    MLISP_DEFUN("car", [cmd](List args, Env& env) {
+        assert_argc(args, 1, cmd);
+        return car(to_list_or_throw(eval(car(args), env), cmd));
+    });
 
-    MLISP_DEFUN("cdr", make_proc([cmd](List args, Env& env) {
-                    assert_argc(args, 1, cmd);
-                    return cdr(to_list_or_throw(eval(car(args), env), cmd));
-                }));
+    MLISP_DEFUN("cdr", [cmd](List args, Env& env) {
+        assert_argc(args, 1, cmd);
+        return cdr(to_list_or_throw(eval(car(args), env), cmd));
+    });
 
-    MLISP_DEFUN("cons", make_proc([cmd](List args, Env& env) {
-                    assert_argc(args, 2, cmd);
-                    auto head = eval(car(args), env);
-                    auto tail = to_list_or_throw(eval(cadr(args), env), cmd);
-                    return cons(head, tail);
-                }));
+    MLISP_DEFUN("cons", [cmd](List args, Env& env) {
+        assert_argc(args, 2, cmd);
+        auto head = eval(car(args), env);
+        auto tail = to_list_or_throw(eval(cadr(args), env), cmd);
+        return cons(head, tail);
+    });
 
-    MLISP_DEFUN("cond", make_proc([cmd](List args, Env& env) {
-                    Node result;
-                    while (!args.empty()) {
-                        auto clause = to_list_or_throw(car(args), cmd);
-                        auto pred = car(clause);
-                        if (to_bool(eval(pred, env))) {
-                            for (auto expr = cdr(clause); !expr.empty(); expr = cdr(expr)) {
-                                result = eval(car(expr), env);
-                            }
-                            return result;
-                        }
-                        args = cdr(args);
-                    }
-                    return result;
-                }));
+    MLISP_DEFUN("cond", [cmd](List args, Env& env) {
+        Node result;
+        while (!args.empty()) {
+            auto clause = to_list_or_throw(car(args), cmd);
+            auto pred = car(clause);
+            if (to_bool(eval(pred, env))) {
+                for (auto expr = cdr(clause); !expr.empty(); expr = cdr(expr)) {
+                    result = eval(car(expr), env);
+                }
+                return result;
+            }
+            args = cdr(args);
+        }
+        return result;
+    });
 
-    MLISP_DEFUN("lambda", make_proc([cmd](List args, Env& env) {
-                    assert_argc_min(args, 2, cmd);
+    MLISP_DEFUN("lambda", [cmd](List args, Env& env) {
+        assert_argc_min(args, 2, cmd);
 
-                    auto formal_args = to_formal_args(car(args), cmd);
-                    auto lambda_body = cadr(args);
-                    auto creator_env = env.shared_from_this();
+        auto formal_args = to_formal_args(car(args), cmd);
+        auto lambda_body = cadr(args);
+        auto creator_env = env.shared_from_this();
 
-                    return make_proc([formal_args, lambda_body, creator_env](List args, Env& env) {
-                        auto lambda_env = creator_env->derive_new();
-                        auto syms = formal_args;
-                        while (!syms.empty()) {
-                            if (args.empty()) {
-                                EvalError("Proc: too few args");
-                            }
+        return make_proc([formal_args, lambda_body, creator_env](List args, Env& env) {
+            auto lambda_env = creator_env->derive_new();
+            auto syms = formal_args;
+            while (!syms.empty()) {
+                if (args.empty()) {
+                    EvalError("Proc: too few args");
+                }
 
-                            auto sym = dynamic_node_cast<Symbol>(car(syms));
-                            auto val = eval(car(args), env);
-                            assert(sym.has_value());
-                            lambda_env->set(sym->name(), val);
-                            syms = cdr(syms);
-                            args = cdr(args);
-                        }
+                auto sym = dynamic_node_cast<Symbol>(car(syms));
+                auto val = eval(car(args), env);
+                assert(sym.has_value());
+                lambda_env->set(sym->name(), val);
+                syms = cdr(syms);
+                args = cdr(args);
+            }
 
-                        if (!args.empty()) {
-                            EvalError("Proc: too many args");
-                        }
+            if (!args.empty()) {
+                EvalError("Proc: too many args");
+            }
 
-                        return eval(lambda_body, *lambda_env);
-                    });
-                }));
+            return eval(lambda_body, *lambda_env);
+        });
+    });
 
-    MLISP_DEFUN("defun", make_proc([](List args, Env& env) {
-                    auto name = car(args);
-                    auto body = cons(Symbol{"lambda"}, cdr(args));
-                    return eval(cons(Symbol{"define"}, cons(name, cons(body, {}))), env);
-                }));
+    MLISP_DEFUN("defun", [](List args, Env& env) {
+        auto name = car(args);
+        auto body = cons(Symbol{"lambda"}, cdr(args));
+        return eval(cons(Symbol{"define"}, cons(name, cons(body, {}))), env);
+    });
 }
 
 void set_complementary_procs(mll::Env& env)
 {
     using namespace mll;
 
-    MLISP_DEFUN("list", make_proc([](List args, Env& env) {
-                    std::vector<Node> objs;
-                    for_each(args, [&objs, &env](auto const& arg) {
-                        objs.push_back(eval(arg, env));
-                    });
-                    while (!args.empty()) {
-                        args = cdr(args);
-                    }
+    MLISP_DEFUN("list", [](List args, Env& env) {
+        std::vector<Node> objs;
+        for_each(args, [&objs, &env](auto const& arg) {
+            objs.push_back(eval(arg, env));
+        });
+        while (!args.empty()) {
+            args = cdr(args);
+        }
 
-                    List list;
-                    while (!objs.empty()) {
-                        list = cons(objs.back(), list);
-                        objs.pop_back();
-                    }
-                    return list;
-                }));
+        List list;
+        while (!objs.empty()) {
+            list = cons(objs.back(), list);
+            objs.pop_back();
+        }
+        return list;
+    });
 
-    MLISP_DEFUN("define", make_proc([cmd](List args, Env& env) {
-                    assert_argc(args, 2, cmd);
+    MLISP_DEFUN("define", [cmd](List args, Env& env) {
+        assert_argc(args, 2, cmd);
 
-                    auto symbol = to_symbol_or_throw(car(args), cmd);
-                    auto value = eval(cadr(args), env);
-                    env.set(symbol.name(), value);
-                    return value;
-                }));
+        auto symbol = to_symbol_or_throw(car(args), cmd);
+        auto value = eval(cadr(args), env);
+        env.set(symbol.name(), value);
+        return value;
+    });
 
-    MLISP_DEFUN("set!", make_proc([cmd](List args, Env& env) {
-                    assert_argc(args, 2, cmd);
+    MLISP_DEFUN("set!", [cmd](List args, Env& env) {
+        assert_argc(args, 2, cmd);
 
-                    auto symbol = to_symbol_or_throw(car(args), cmd);
-                    auto value = eval(cadr(args), env);
-                    if (!env.update(symbol.name(), value)) {
-                        throw EvalError("unbound variable: " + symbol.name());
-                    }
-                    return value;
-                }));
+        auto symbol = to_symbol_or_throw(car(args), cmd);
+        auto value = eval(cadr(args), env);
+        if (!env.update(symbol.name(), value)) {
+            throw EvalError("unbound variable: " + symbol.name());
+        }
+        return value;
+    });
 
-    MLISP_DEFUN("begin", make_proc([/*cmd*/](List args, Env& env) {
-                    auto new_env = env.derive_new();
-                    Node value;
-                    while (!args.empty()) {
-                        value = eval(car(args), *new_env);
-                        args = cdr(args);
-                    }
-                    return value;
-                }));
+    MLISP_DEFUN("begin", [/*cmd*/](List args, Env& env) {
+        auto new_env = env.derive_new();
+        Node value;
+        while (!args.empty()) {
+            value = eval(car(args), *new_env);
+            args = cdr(args);
+        }
+        return value;
+    });
 
-    MLISP_DEFUN("if", make_proc([cmd](List args, Env& env) {
-                    assert_argc_range(args, 2, 3, cmd);
+    MLISP_DEFUN("if", [cmd](List args, Env& env) {
+        assert_argc_range(args, 2, 3, cmd);
 
-                    auto cond = car(args);
-                    auto body = cdr(args);
-                    auto then_arm = car(body);
-                    auto else_arm = cadr(body);
-                    if (to_bool(eval(cond, env))) {
-                        return eval(then_arm, env);
-                    }
-                    return eval(else_arm, env);
-                }));
+        auto cond = car(args);
+        auto body = cdr(args);
+        auto then_arm = car(body);
+        auto else_arm = cadr(body);
+        if (to_bool(eval(cond, env))) {
+            return eval(then_arm, env);
+        }
+        return eval(else_arm, env);
+    });
 
-    MLISP_DEFUN("print", make_proc([/*cmd*/](List const& args, Env& env) {
-                    for_each_with_index(args, [&env](auto const i, auto const& expr) {
-                        if (i != 0) {
-                            std::cout << ' ';
-                        }
-                        print(std::cout, eval(expr, env), StringStyle::raw);
-                    });
-                    std::cout << '\n';
-                    return nil;
-                }));
+    MLISP_DEFUN("print", [/*cmd*/](List const& args, Env& env) {
+        for_each_with_index(args, [&env](auto const i, auto const& expr) {
+            if (i != 0) {
+                std::cout << ' ';
+            }
+            print(std::cout, eval(expr, env), StringStyle::raw);
+        });
+        std::cout << '\n';
+        return nil;
+    });
 
-    MLISP_DEFUN("load", make_proc([cmd](List args, Env& env) {
-                    assert_argc(args, 1, cmd);
-                    auto filename = to_string_or_throw(car(args), cmd);
-                    return to_node(!load_file(env, filename.text()));
-                }));
+    MLISP_DEFUN("load", [cmd](List args, Env& env) {
+        assert_argc(args, 1, cmd);
+        auto filename = to_string_or_throw(car(args), cmd);
+        return to_node(!load_file(env, filename.text()));
+    });
 }
 
 void set_number_procs(mll::Env& env)
 {
     using namespace mll;
 
-    MLISP_DEFUN("number?", make_proc([cmd](List args, Env& env) {
-                    assert_argc(args, 1, cmd);
-                    return to_node(is_number(eval(car(args), env)));
-                }));
+    MLISP_DEFUN("number?", [cmd](List args, Env& env) {
+        assert_argc(args, 1, cmd);
+        return to_node(is_number(eval(car(args), env)));
+    });
 
-    MLISP_DEFUN("number-equal?", make_proc([cmd](List args, Env& env) {
-                    assert_argc(args, 2, cmd);
+    MLISP_DEFUN("number-equal?", [cmd](List args, Env& env) {
+        assert_argc(args, 2, cmd);
 
-                    auto num1 = to_number_or_throw(eval(car(args), env), cmd);
-                    auto num2 = to_number_or_throw(eval(cadr(args), env), cmd);
+        auto num1 = to_number_or_throw(eval(car(args), env), cmd);
+        auto num2 = to_number_or_throw(eval(cadr(args), env), cmd);
 
-                    return to_node(num1.value() == num2.value());
-                }));
+        return to_node(num1.value() == num2.value());
+    });
 
-    MLISP_DEFUN("number-less?", make_proc([cmd](List args, Env& env) {
-                    assert_argc(args, 2, cmd);
+    MLISP_DEFUN("number-less?", [cmd](List args, Env& env) {
+        assert_argc(args, 2, cmd);
 
-                    auto num1 = to_number_or_throw(eval(car(args), env), cmd);
-                    auto num2 = to_number_or_throw(eval(cadr(args), env), cmd);
-                    return to_node(num1.value() < num2.value());
-                }));
+        auto num1 = to_number_or_throw(eval(car(args), env), cmd);
+        auto num2 = to_number_or_throw(eval(cadr(args), env), cmd);
+        return to_node(num1.value() < num2.value());
+    });
 
-    MLISP_DEFUN("+", make_proc([cmd](List args, Env& env) {
-                    auto result = 0.0;
-                    for_each(args, [&result, &env, cmd](auto const& arg) {
-                        result += to_number_or_throw(eval(arg, env), cmd).value();
-                    });
-                    return Number{result};
-                }));
+    MLISP_DEFUN("+", [cmd](List args, Env& env) {
+        auto result = 0.0;
+        for_each(args, [&result, &env, cmd](auto const& arg) {
+            result += to_number_or_throw(eval(arg, env), cmd).value();
+        });
+        return Number{result};
+    });
 
-    MLISP_DEFUN("-", make_proc([cmd](List args, Env& env) {
-                    assert_argc_min(args, 1, cmd);
+    MLISP_DEFUN("-", [cmd](List args, Env& env) {
+        assert_argc_min(args, 1, cmd);
 
-                    auto result = to_number_or_throw(eval(car(args), env), cmd).value();
-                    args = cdr(args);
-                    if (args.empty()) {
-                        // unary minus
-                        result = -result;
-                    }
-                    else {
-                        for_each(args, [&result, &env, cmd](auto const& arg) {
-                            result -= to_number_or_throw(eval(arg, env), cmd).value();
-                        });
-                    }
-                    return Number{result};
-                }));
+        auto result = to_number_or_throw(eval(car(args), env), cmd).value();
+        args = cdr(args);
+        if (args.empty()) {
+            // unary minus
+            result = -result;
+        }
+        else {
+            for_each(args, [&result, &env, cmd](auto const& arg) {
+                result -= to_number_or_throw(eval(arg, env), cmd).value();
+            });
+        }
+        return Number{result};
+    });
 
-    MLISP_DEFUN("*", make_proc([cmd](List args, Env& env) {
-                    auto result = 1.0;
-                    while (!args.empty()) {
-                        auto arg = eval(car(args), env);
-                        result *= to_number_or_throw(arg, cmd).value();
-                        args = cdr(args);
-                    }
-                    return Number{result};
-                }));
+    MLISP_DEFUN("*", [cmd](List args, Env& env) {
+        auto result = 1.0;
+        while (!args.empty()) {
+            auto arg = eval(car(args), env);
+            result *= to_number_or_throw(arg, cmd).value();
+            args = cdr(args);
+        }
+        return Number{result};
+    });
 
-    MLISP_DEFUN("/", make_proc([cmd](List args, Env& env) {
-                    assert_argc_min(args, 2, cmd);
+    MLISP_DEFUN("/", [cmd](List args, Env& env) {
+        assert_argc_min(args, 2, cmd);
 
-                    auto result = to_number_or_throw(eval(car(args), env), cmd).value();
-                    for_each(cdr(args), [&result, &env, cmd](auto const& arg) {
-                        result /= to_number_or_throw(eval(arg, env), cmd).value();
-                    });
-                    return Number{result};
-                }));
+        auto result = to_number_or_throw(eval(car(args), env), cmd).value();
+        for_each(cdr(args), [&result, &env, cmd](auto const& arg) {
+            result /= to_number_or_throw(eval(arg, env), cmd).value();
+        });
+        return Number{result};
+    });
 }
 
 void set_string_procs(mll::Env& env)
 {
-    MLISP_DEFUN("string?", make_proc([cmd](mll::List const& args, mll::Env& env) {
-                    assert_argc(args, 1, cmd);
-                    return to_node(is_string(eval(car(args), env)));
-                }));
+    MLISP_DEFUN("string?", [cmd](mll::List const& args, mll::Env& env) {
+        assert_argc(args, 1, cmd);
+        return to_node(is_string(eval(car(args), env)));
+    });
 
-    MLISP_DEFUN("string-equal?", make_proc([cmd](mll::List const& args, mll::Env& env) {
-                    assert_argc(args, 2, cmd);
+    MLISP_DEFUN("string-equal?", [cmd](mll::List const& args, mll::Env& env) {
+        assert_argc(args, 2, cmd);
 
-                    auto const str1 = to_string_or_throw(eval(car(args), env), cmd);
-                    auto const str2 = to_string_or_throw(eval(cadr(args), env), cmd);
-                    return to_node(str1.text() == str2.text());
-                }));
+        auto const str1 = to_string_or_throw(eval(car(args), env), cmd);
+        auto const str2 = to_string_or_throw(eval(cadr(args), env), cmd);
+        return to_node(str1.text() == str2.text());
+    });
 }
 
 void set_symbol_procs(mll::Env& env)
 {
-    MLISP_DEFUN("symbol?", make_proc([cmd](mll::List const& args, mll::Env& env) {
-                    assert_argc(args, 1, cmd);
-                    return to_node(is_symbol(eval(car(args), env)));
-                }));
+    MLISP_DEFUN("symbol?", [cmd](mll::List const& args, mll::Env& env) {
+        assert_argc(args, 1, cmd);
+        return to_node(is_symbol(eval(car(args), env)));
+    });
 }
 
 } // namespace mlisp
--- tools/clang-format-all.sh
+++ tools/clang-format-all.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-directories="src"
-# Assume clang-format.sh resides in the same directory as clang-format-all.sh
-scriptDirectory="$(cd "$(dirname "$0")" && pwd -P)"
-find ${directories} -type f -print | xargs "${scriptDirectory}"/clang-format.sh
+scriptDir="$(cd "$(dirname "$0")" && pwd -P)"
+find "src" -type f -print | xargs "${scriptDir}"/clang-format.sh
