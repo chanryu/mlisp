1384436724bc5211bb7eb380f9c7052ce8987e99
Chan Ryu <upriser@gmail.com>
2019-11-12 02:22:36 +1100

    wip

--- tools/configure.sh
+++ tools/configure.sh
@@ -2,4 +2,4 @@
 
 ROOT=$(git rev-parse --show-toplevel)
 BUILD="$ROOT/build"
-mkdir -p "$BUILD" && pushd "$BUILD" && cmake -GNinja .. && popd
+mkdir -p "$BUILD" && pushd "$BUILD" && cmake -G "Unix Makefiles" .. && popd
