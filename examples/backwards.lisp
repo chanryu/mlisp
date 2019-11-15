269fb4765359e20c6788830b036dd4b26b1f1ea5
Chan Ryu <upriser@gmail.com>
2019-11-16 00:09:20 +1100

    update examples

--- examples/backwards.lisp
+++ examples/backwards.lisp
@@ -1,10 +1,10 @@
 (load "primitives.lisp")
 
-(defun reverse (lst)
+(define (reverse lst)
     (cond ((null? lst) lst)
           ('t (append (reverse (cdr lst)) (list (car lst))))))
 
-(defmacro backwards (*args)
+(define-macro (backwards *args)
     `(begin ,@(reverse args)))
 
 (backwards (print 1) (print 2) (print 3))
--- examples/primitives.lisp
+++ examples/primitives.lisp
@@ -3,9 +3,14 @@
     (macro (name args body)
         `(define ,name (macro ,args ,body))))
 
+; define-macro
+(define define-macro
+    (macro (name_args body)
+        `(define ,(car name_args) (macro ,(cdr name_args) ,body))))
+
 ; defun
 (defmacro defun (name args body)
-    `(define ,name (lambda ,args ,body)))
+    `(define (,name ,@args) ,body))
 
 ; list
 (defun list (*args) args)
